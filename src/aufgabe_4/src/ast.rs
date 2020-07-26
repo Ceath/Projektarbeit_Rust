c0dcfa842f65641d0dfaaf92342f89064f443bb1
Daniel <dada1016@hs-karlsruhe.de>
2020-07-26 16:28:05 +0200

    added unit-tests for ast

--- src/aufgabe_4/src/ast.rs
+++ src/aufgabe_4/src/ast.rs
@@ -1,6 +1,6 @@
 use std::string::String;
 
-
+#[derive(Eq, PartialEq, Debug)]
 pub enum ExprType {
     Int,
     Plus,
@@ -14,9 +14,8 @@ pub trait Expr {
     fn pretty_clever(&self) -> String;
 }
 
-
 pub struct IntExpr {
-    pub val: i32
+    pub val: i32,
 }
 
 impl Expr for IntExpr {
@@ -32,18 +31,16 @@ impl Expr for IntExpr {
         ExprType::Int
     }
 
-    fn pretty_clever(&self) -> String {self.pretty()}
-
+    fn pretty_clever(&self) -> String {
+        self.pretty()
+    }
 }
 
 impl IntExpr {
     pub fn new(val: i32) -> IntExpr {
-        IntExpr {
-            val
-        }
+        IntExpr { val }
     }
 }
-
 
 pub struct PlusExpr<T: Expr, U: Expr> {
     pub e1: T,
@@ -64,29 +61,18 @@ impl<T: Expr, U: Expr> Expr for PlusExpr<T, U> {
     }
 
     fn pretty_clever(&self) -> String {
-        fn pretty_val<E: Expr>(e: &E) -> String {
-            if let ExprType::Mul = e.expr_type() {
-                format!("({})",e.pretty_clever())
-            }
-            else { e.pretty_clever()}
-        }
-
-        let e1_pretty = pretty_val(&self.e1);
-        let e2_pretty = pretty_val(&self.e2);
+        let e1_pretty = self.e1.pretty_clever();
+        let e2_pretty = self.e2.pretty_clever();
 
         format!("{} + {}", e1_pretty, e2_pretty)
     }
 }
 
 impl<T: Expr, U: Expr> PlusExpr<T, U> {
-    pub fn new(e1: T, e2: U) -> PlusExpr<T,U> {
-        PlusExpr {
-            e1,
-            e2
-        }
+    pub fn new(e1: T, e2: U) -> PlusExpr<T, U> {
+        PlusExpr { e1, e2 }
     }
 }
-
 
 pub struct MulExpr<T: Expr, U: Expr> {
     pub e1: T,
@@ -109,8 +95,10 @@ impl<T: Expr, U: Expr> Expr for MulExpr<T, U> {
     fn pretty_clever(&self) -> String {
         fn pretty_val<E: Expr>(e: &E) -> String {
             if let ExprType::Plus = e.expr_type() {
-                format!("({})", e.pretty_clever())}
-            else {e.pretty_clever()}
+                format!("({})", e.pretty_clever())
+            } else {
+                e.pretty_clever()
+            }
         }
 
         let e1_pretty = pretty_val(&self.e1);
@@ -120,11 +108,255 @@ impl<T: Expr, U: Expr> Expr for MulExpr<T, U> {
     }
 }
 
-impl<T: Expr, U: Expr> MulExpr<T,U> {
-    pub fn new(e1: T, e2: U) -> MulExpr<T,U> {
-        MulExpr {
-            e1,
-            e2
+impl<T: Expr, U: Expr> MulExpr<T, U> {
+    pub fn new(e1: T, e2: U) -> MulExpr<T, U> {
+        MulExpr { e1, e2 }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //expr_type()
+    mod expr_type {
+        use super::*;
+
+        #[test]
+        fn expr_type_int() {
+            let e = IntExpr::new(0);
+
+            assert_eq!(ExprType::Int, e.expr_type());
+        }
+
+        #[test]
+        fn expr_type_plus() {
+            let e = PlusExpr::new(IntExpr::new(0), IntExpr::new(1));
+
+            assert_eq!(ExprType::Plus, e.expr_type());
+        }
+
+        #[test]
+        fn expr_type_mul() {
+            let e = MulExpr::new(IntExpr::new(0), IntExpr::new(1));
+
+            assert_eq!(ExprType::Mul, e.expr_type());
+        }
+    }
+
+    // eval()
+    mod eval {
+        use super::*;
+
+        #[test]
+        fn eval_int() {
+            let int = IntExpr::new(10);
+
+            assert_eq!(int.eval(), 10);
+        }
+
+        #[test]
+        fn eval_plus() {
+            // (10 + 5)
+            let e = PlusExpr::new(IntExpr::new(10), IntExpr::new(5));
+
+            assert_eq!(e.eval(), 15);
+        }
+
+        #[test]
+        fn eval_plus_nested() {
+            // ((1 + 2) + 3)
+            let e = PlusExpr::new(
+                PlusExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.eval(), 6);
+        }
+
+        #[test]
+        fn eval_mul() {
+            // (10 * 5)
+            let e = MulExpr::new(IntExpr::new(10), IntExpr::new(5));
+
+            assert_eq!(e.eval(), 50);
+        }
+
+        #[test]
+        fn eval_mul_nested() {
+            // ((1 * 2) * 3)
+            let e = MulExpr::new(
+                MulExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.eval(), 6);
+        }
+
+        #[test]
+        fn eval_nested_mul_plus() {
+            // ((1 + 2) * 3)
+            let e = MulExpr::new(
+                PlusExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.eval(), 9);
+        }
+
+        #[test]
+        fn eval_nested_plus_plus() {
+            // ((1 * 2) + 3)
+            let e = PlusExpr::new(
+                MulExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.eval(), 5);
+        }
+    }
+
+    // pretty()
+    mod pretty {
+        use super::*;
+
+        #[test]
+        fn pretty_int() {
+            let e = IntExpr::new(5);
+
+            assert_eq!(e.pretty(), "5");
+        }
+
+        #[test]
+        fn pretty_plus() {
+            // (10 + 5)
+            let e = PlusExpr::new(IntExpr::new(10), IntExpr::new(5));
+
+            assert_eq!(e.pretty(), "(10 + 5)");
+        }
+
+        #[test]
+        fn pretty_plus_nested() {
+            // ((1 + 2) + 3)
+            let e = PlusExpr::new(
+                PlusExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty(), "((1 + 2) + 3)");
+        }
+
+        #[test]
+        fn pretty_mul() {
+            // (10 * 5)
+            let e = MulExpr::new(IntExpr::new(10), IntExpr::new(5));
+
+            assert_eq!(e.pretty(), "(10 * 5)");
+        }
+
+        #[test]
+        fn pretty_mul_nested() {
+            // ((1 * 2) * 3)
+            let e = MulExpr::new(
+                MulExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty(), "((1 * 2) * 3)");
+        }
+
+        #[test]
+        fn pretty_nested_mul_plus() {
+            // ((1 + 2) * 3)
+            let e = MulExpr::new(
+                PlusExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty(), "((1 + 2) * 3)");
+        }
+
+        #[test]
+        fn pretty_nested_plus_plus() {
+            // ((1 * 2) + 3)
+            let e = PlusExpr::new(
+                MulExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty(), "((1 * 2) + 3)");
+        }
+    }
+
+    //pretty_clever()
+    mod pretty_clever {
+        use super::*;
+
+        #[test]
+        fn pretty_clever_int() {
+            let e = IntExpr::new(5);
+
+            assert_eq!(e.pretty_clever(), "5");
+        }
+
+        #[test]
+        fn pretty_clever_plus() {
+            // (10 + 5)
+            let e = PlusExpr::new(IntExpr::new(10), IntExpr::new(5));
+
+            assert_eq!(e.pretty_clever(), "10 + 5");
+        }
+
+        #[test]
+        fn pretty_clever_plus_nested() {
+            // ((1 + 2) + 3)
+            let e = PlusExpr::new(
+                PlusExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty_clever(), "1 + 2 + 3");
+        }
+
+        #[test]
+        fn pretty_clever_mul() {
+            // (10 * 5)
+            let e = MulExpr::new(IntExpr::new(10), IntExpr::new(5));
+
+            assert_eq!(e.pretty_clever(), "10 * 5");
+        }
+
+        #[test]
+        fn pretty_clever_mul_nested() {
+            // ((1 * 2) * 3)
+            let e = MulExpr::new(
+                MulExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty_clever(), "1 * 2 * 3");
+        }
+
+        #[test]
+        fn pretty_clever_nested_mul_plus() {
+            // ((1 + 2) * 3)
+            let e = MulExpr::new(
+                PlusExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty_clever(), "(1 + 2) * 3");
+        }
+
+        #[test]
+        fn pretty_clever_nested_plus_plus() {
+            // ((1 * 2) + 3)
+            let e = PlusExpr::new(
+                MulExpr::new(IntExpr::new(1), IntExpr::new(2)),
+                IntExpr::new(3),
+            );
+
+            assert_eq!(e.pretty_clever(), "1 * 2 + 3");
         }
     }
 }
--- src/aufgabe_4/src/main.rs
+++ src/aufgabe_4/src/main.rs
@@ -1,17 +1,14 @@
 mod ast;
 
-use ast::{IntExpr, PlusExpr, MulExpr, Expr};
+use ast::{Expr, IntExpr, MulExpr, PlusExpr};
 
 fn main() {
-    fn print<T: Expr>(expr: &T){
-        println!("{} = {}", expr.pretty(), expr.eval());
-        println!("clever: {} = {}", expr.pretty_clever(), expr.eval());
+    fn print<T: Expr>(expr: &T) {
+        println!("Expr '{}' to clever:= '{}', eval:= {}", expr.pretty(), expr.pretty_clever(), expr.eval());
     }
 
     let plus = PlusExpr {
-        e1: IntExpr {
-            val: 5
-        },
+        e1: IntExpr { val: 5 },
         e2: PlusExpr {
             e1: IntExpr { val: 10 },
             e2: IntExpr { val: 3 },
@@ -30,33 +27,31 @@ fn main() {
     println!();
 
     let ex1 = PlusExpr::new(
-        IntExpr::new(1), MulExpr::new(
-            IntExpr::new(3), IntExpr::new(5)
-        ));
+        IntExpr::new(1),
+        MulExpr::new(IntExpr::new(3), IntExpr::new(5)),
+    );
     print(&ex1);
 
     let ex2 = MulExpr::new(
-        IntExpr::new(1), PlusExpr::new(
-            IntExpr::new(3), IntExpr::new(5)
-        )
+        IntExpr::new(1),
+        PlusExpr::new(IntExpr::new(3), IntExpr::new(5)),
     );
     print(&ex2);
     eprintln!();
 
     let ex_eq1 = PlusExpr::new(
-        IntExpr::new(1), PlusExpr::new(
-            IntExpr::new(2), IntExpr::new(3)
-        )
+        IntExpr::new(1),
+        PlusExpr::new(IntExpr::new(2), IntExpr::new(3)),
     );
     let ex_eq2 = PlusExpr::new(
-        PlusExpr::new(
-            IntExpr::new(1), IntExpr::new(2)
-        ),
-        IntExpr::new(3)
+        PlusExpr::new(IntExpr::new(1), IntExpr::new(2)),
+        IntExpr::new(3),
     );
 
     print(&ex_eq1);
     print(&ex_eq2);
-    println!("ex_eq1 == ex_eq2:= {}", ex_eq1.pretty_clever() == ex_eq2.pretty_clever());
-
+    println!(
+        "ex_eq1.pretty_clever() == ex_eq2.pretty_clever():= {}",
+        ex_eq1.pretty_clever() == ex_eq2.pretty_clever()
+    );
 }
